e096233519bf8f77920a22e5102b09baff98c424
Pramudya Aji <97204740+prammmaji@users.noreply.github.com>
2024-01-23 15:39:37 +0700

    Update koneksi.js

--- config/koneksi.js
+++ config/koneksi.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require("sequelize");
-
+const mysql2 = require("mysql2");
 export const db = new Sequelize('freedb_pembayaran_spp', 'freedb_user-new', 'cDzU6$7C%pH!&S*' , {
     host: 'sql.freedb.tech',
     port: 3306,
-    dialect: 'mysql'
+    dialect: 'mysql',
+    dialectModule: 'mysql2';
 })
 
