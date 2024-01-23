3bff97e311ed6254e0923e55d083dbb8850649a6
Pramudya Aji <97204740+prammmaji@users.noreply.github.com>
2024-01-23 19:35:15 +0700

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,15 +1,21 @@
-import express from "express";
-import { getUser, getUserById, createUser,createAdmin, updateUser, deleteUser } from "../controllers/userController.js";
-import { verifyUser, adminOnly } from "../middleware/AuthUser.js"
+const express = require("express");
+const {
+  getUser,
+  getUserById,
+  createUser,
+  createAdmin,
+  updateUser,
+  deleteUser
+} = require("../controllers/userController.js");
+const { verifyUser, adminOnly } = require("../middleware/AuthUser.js");
 
 const router = express.Router();
 
-router.get("/users", verifyUser,adminOnly, getUser);
-router.get("/users/:id", verifyUser,getUserById);
-router.post("/users", verifyUser,adminOnly,createUser);
-router.post("/admin", verifyUser,adminOnly,createAdmin);
-router.patch("/users/:id", verifyUser,updateUser);
-router.delete("/users/:id",verifyUser,adminOnly, deleteUser);
+router.get("/users", verifyUser, adminOnly, getUser);
+router.get("/users/:id", verifyUser, getUserById);
+router.post("/users", verifyUser, adminOnly, createUser);
+router.post("/admin", verifyUser, adminOnly, createAdmin);
+router.patch("/users/:id", verifyUser, updateUser);
+router.delete("/users/:id", verifyUser, adminOnly, deleteUser);
 
-
-export default router;
+module.exports = router;
