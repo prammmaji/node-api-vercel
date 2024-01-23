d3fc1bf19e20ba85cadafe8b44800302f8767f19
Pramudya Aji <97204740+prammmaji@users.noreply.github.com>
2024-01-23 19:24:37 +0700

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -15,8 +15,8 @@ const laporanRoute = require("./routes/laporanRoute.js");
 const SequelizeStore = require("connect-session-sequelize");
 
 
-dotenv.config()
-const app = express()
+dotenv.config();
+const app = express();
 (async () => {
   try {
     await db.authenticate();
@@ -28,13 +28,13 @@ const app = express()
 })();
 app.use('/public/images', express.static(imagePath));
 
-const sessionStore = SequelizeStore(session.Store)
+const sessionStore = SequelizeStore(session.Store);
 
 const store = new sessionStore({
     db:db,
-})
+});
 
-app.use(bodyParser.urlencoded({extended:true})) 
+app.use(bodyParser.urlencoded({extended:true})); 
 
 app.use(session({
     secret:process.env.SESS_SECRET,
@@ -44,23 +44,23 @@ app.use(session({
     cookie: {
         secure: 'auto'
     }
-}))
+}));
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
-})
+});
 app.use(cors({
     credentials:true,
     origin:'http://localhost:3000'
-}))
-app.use(cookieParse())
-app.use(express.json())
-app.use(userRoute)
-app.use(pembayaranRoute)
-app.use(tagihanRoute)
-app.use(siswaRoute)
-app.use(authRoute)
-app.use(laporanRoute)
+}));
+app.use(cookieParse());
+app.use(express.json());
+app.use(userRoute);
+app.use(pembayaranRoute);
+app.use(tagihanRoute);
+app.use(siswaRoute);
+app.use(authRoute);
+app.use(laporanRoute);
 
-store.sync()
+store.sync();
 
-app.listen(process.env.APP_PORT,() => console.log("Server running on port 5000"))
+app.listen(process.env.APP_PORT,() => console.log("Server running on port 5000"));
