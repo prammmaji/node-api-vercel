ea58fece9adcb45d5370512b31e4f230b7abec0d
Pramudya Aji <97204740+prammmaji@users.noreply.github.com>
2024-01-23 12:00:38 +0700

    Update koneksi.js

--- config/koneksi.js
+++ config/koneksi.js
@@ -1,4 +1,5 @@
 import { Sequelize } from "sequelize"
+import mysql from "mysql2";
 
 export const db = new Sequelize('freedb_pembayaran_spp', 'freedb_user-new', 'cDzU6$7C%pH!&S*' , {
     host: 'sql.freedb.tech',
