a9f45f4b94c2360f143fa7e40cd3d480bf0fbed6
Pramudya Aji <97204740+prammmaji@users.noreply.github.com>
2024-01-23 10:54:32 +0700

    Update koneksi.js

--- config/koneksi.js
+++ config/koneksi.js
@@ -1,10 +1,8 @@
 import { Sequelize } from "sequelize"
 
-const db = new Sequelize('freedb_pembayaran_spp', 'freedb_user-new', 'cDzU6$7C%pH!&S*' , {
+export const db = new Sequelize('freedb_pembayaran_spp', 'freedb_user-new', 'cDzU6$7C%pH!&S*' , {
     host: 'sql.freedb.tech',
     port: 3306,
     dialect: 'mysql'    
 })
-
-export default db;
 
