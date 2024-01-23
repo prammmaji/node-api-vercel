59d0af54433705f07ddeb67ba908a17c47ad6cc7
Pramudya Aji <97204740+prammmaji@users.noreply.github.com>
2024-01-23 19:34:45 +0700

    Update tagihanRoute.js

--- routes/tagihanRoute.js
+++ routes/tagihanRoute.js
@@ -1,15 +1,21 @@
-import express from "express";
-import { getTagihan,createTagihan,getTagihanById,updateTagihan,deleteTagihan,getTagihanAktif } from "../controllers/tagihanConttollers.js";
-import { verifyUser, adminOnly } from "../middleware/AuthUser.js"
+const express = require("express");
+const {
+  getTagihan,
+  createTagihan,
+  getTagihanById,
+  updateTagihan,
+  deleteTagihan,
+  getTagihanAktif
+} = require("../controllers/tagihanConttollers.js");
+const { verifyUser, adminOnly } = require("../middleware/AuthUser.js");
 
 const router = express.Router();
 
-router.get("/tagihan/all", verifyUser,adminOnly, getTagihan);
-router.get("/tagihan/aktif", verifyUser,getTagihanAktif);
-router.get("/tagihan/:id", verifyUser,getTagihanById);
-router.post("/tagihan", verifyUser,adminOnly,createTagihan);
-router.patch("/tagihan/:id", verifyUser,adminOnly,updateTagihan);
-router.delete("/tagihan/:id",verifyUser,adminOnly, deleteTagihan);
+router.get("/tagihan/all", verifyUser, adminOnly, getTagihan);
+router.get("/tagihan/aktif", verifyUser, getTagihanAktif);
+router.get("/tagihan/:id", verifyUser, getTagihanById);
+router.post("/tagihan", verifyUser, adminOnly, createTagihan);
+router.patch("/tagihan/:id", verifyUser, adminOnly, updateTagihan);
+router.delete("/tagihan/:id", verifyUser, adminOnly, deleteTagihan);
 
-
-export default router;
+module.exports = router;
