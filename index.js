23672aa5263647e3a03e160bb15c48448c97fd7b
Pramudya Aji <97204740+prammmaji@users.noreply.github.com>
2024-01-23 10:51:00 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
+
 import express from "express"
 import db from "./config/koneksi.js"
 // import router from "./routes/index.js"
